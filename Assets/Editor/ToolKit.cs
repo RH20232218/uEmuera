974adc3ac443ebc5c053944cdadcb623cb672f22
RH20232218 <exy416@gmail.com>
2025-06-22 11:33:16 +0900

    a

--- Assets/Editor/ToolKit.cs
+++ Assets/Editor/ToolKit.cs
@@ -6,7 +6,7 @@ using System.Collections;
 public class ToolKit : EditorWindow 
 {
     [MenuItem("Tools/ToolKit", false, 100)]
-    public static void Show()
+    public static new void Show()
     {
         EditorWindow.GetWindow(typeof(ToolKit));
     }
